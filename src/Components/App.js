41eaeb2be3f4fc00c96bcd6122292063300c7e37
FareedaRagab <fareeeeda1@gmail.com>
2022-08-19 16:38:45 +0200

    Revert "Browser tab icon changing according to weather"
    This reverts commit 8fd9b9cda4a03e70d1d63fe0feddb994cf9798cd.

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,20 +8,6 @@ import RequiredItems from "./RequiredItems";
 import Map from "./Map";
 import About from "./About";
 import Footer from "./Footer";
-// Imports for changing the browser icon dynamically
-import Sunny from '../assets/icons/sun.png'
-import Rain from '../assets/icons/rain.png'
-import Clouds from '../assets/icons/clouds.png'
-import ThunderStorm from '../assets/icons/thunderstorm.png'
-import Tornado from '../assets/icons/tornado.png'
-import SandStorm from '../assets/icons/sand_storm.png'
-import Sand from '../assets/icons/sand.png'
-import Mist from '../assets/icons/mist.png'
-import Haze from '../assets/icons/haze.png'
-import Drizzle from '../assets/icons/drizzle.png'
-import Snowy from '../assets/icons/snowy.png'
-import Smoke from '../assets/icons/smoke.png'
-import Logo from '../assets/icons/logo.png'
 
 export default function App() {
   const [error, setError] = useState(null);
@@ -92,62 +78,6 @@ export default function App() {
 
     return () => clearTimeout(fetchData)
   }, [city])
-
-
-  // Change browser icon drnamically
-  useEffect(() => {  
-    const faviconUpdate = async () => {
-      const favicon = document.getElementById("favicon");
-      if (results) {
-        switch(results.weather[0].main) {
-          case 'Rain':
-            favicon.href = Rain;
-            break;
-          case 'Snow': 
-            favicon.href = Snowy;
-            break;
-          case 'Clear':
-            favicon.href = Sunny;
-            break;
-          case 'Clouds':
-            favicon.href = Clouds;
-            break;
-          case 'Tornado':
-            favicon.href = Tornado;
-            break;
-          case 'Drizzle':
-            favicon.href = Drizzle;
-            break;
-          case 'Thunderstorm':
-            favicon.href = ThunderStorm;
-            break;
-          case 'Squall':
-          case 'Ash':
-          case 'Dust':
-            favicon.href = SandStorm;
-            break;
-          case 'Sand':
-            favicon.href = Sand;
-            break;
-          case 'Haze':
-            favicon.href = Haze;
-            break;
-          case 'Smoke':
-            favicon.href = Smoke;
-            break;
-          case 'Mist':
-          case 'Fog':
-            favicon.href = Mist;
-            break;
-          default:
-            favicon.href = Logo;
-            break;
-        }
-      } 
-    };
-
-    faviconUpdate();  
-  }, [results]);
 
   if (error) {
     return <ErrorComponent error={error} />;
